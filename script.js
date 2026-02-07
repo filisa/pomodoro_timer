a38d2ebf68cb9ef9c0850b09f9d256639f4f075b
Margarita <45718204+filisa@users.noreply.github.com>
2026-02-07 22:01:05 +0100

    Pomodoro timer now works

---
+++ script.js
@@ -0,0 +1,71 @@
+const timer = document.getElementById('timer')
+const startBtn = document.getElementById('start-btn')
+const stopBtn = document.getElementById('stop-btn')
+const pauseBtn = document.getElementById('pause-btn')
+
+stopBtn.classList.add('hidden')
+let intervalTimer = null
+let second = 0
+let minute = 25
+
+//start button
+startBtn.addEventListener('click', function(){
+    if (intervalTimer === null) {
+        stopBtn.classList.remove('hidden')
+        startBtn.classList.add('hidden')
+        updateTime()
+    }
+})
+
+//stop button
+stopBtn.addEventListener('click', function(){
+    if (intervalTimer !== null) {
+        stopTimer()
+        renderTime()
+    }
+})
+
+//pause button
+pauseBtn.addEventListener('click', function(){
+    document.body.style.background =  "#51859d"
+})
+
+//what happens if Stop button is clicked or the times runs out of time
+function stopTimer() {
+    clearInterval(intervalTimer)
+    intervalTimer = null
+    second = 0
+    minute = 25
+    startBtn.classList.remove('hidden')//show start button
+    stopBtn.classList.add('hidden')//hide stop button
+}
+
+//updates the timer every second
+function updateTime() {
+    intervalTimer = setInterval(function() {
+        if(second > 0 && second < 60) {
+            second -= 1
+        } else if (minute > 0) {
+            minute = minute - 1
+            second = 59
+        } else {
+            stopTimer()
+        }
+        renderTime()
+    }, 1000)
+    
+}
+
+//renders the timer on the page
+function renderTime() {
+    let minuteString = minute
+    let secondString = second 
+    if (secondString < 10) {
+        secondString = `0${secondString}`
+    } 
+    if (minuteString < 10) {
+        minuteString = `0${minuteString}`
+    }
+    timer.innerText = `${minuteString}:${secondString}`
+}
+
