f151d9bfde48e64e0b820de61af1ebe661a65f26
Margarita Nikulina <ritan@fr24.com>
2026-02-08 13:08:22 +0100

    now break timer can be set for 5 minutes, it can be stopped and paused
    There are some bugs though:
    1. If I click Start on the main timer and then pause, if I click start again, the time starts from the start:(
    2. If I click pause and then start, the color of the text doesn't change
    3. If I open Break and click stop, then the thing should hide

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ const startBtn = document.getElementById('start-btn')
 const pauseBtn = document.getElementById('pause-btn')
 const breakBtn = document.getElementById('break-btn')
 const breakPopup = document.getElementById('break-popup')
+const startBreakTimerBtn = document.getElementById('start-break-timer-btn')
+const stopBreakTimerBtn = document.getElementById('stop-break-timer-btn')
+const timerBreak = document.getElementById('timer-break')
 
 //stopBtn.classList.add('hidden')
 let intervalTimer = null
 let second = 0
-let minute = 25
+let minuteFocusTime = 25
+let minuteBreak = 5
 
-//start button
+breakPopup.style.display = 'none'
+
+//start button for focus time
 startBtn.addEventListener('click', function(){
     if (intervalTimer === null) {
         document.body.classList.add('animation') 
-        updateTime()
+        updateTime(second,minuteFocusTime,timer)
+        renderTime(second,minuteFocusTime, timer)
+        //stopTimer(second,minuteFocusTime)
     }
 })
 
-//pause button
+//pause button for focus time
 pauseBtn.addEventListener('click', function(){
     document.body.style.background =  "#51859d"
     document.body.style.color = "#3a6f88"
@@ -25,9 +33,11 @@ pauseBtn.addEventListener('click', function(){
     intervalTimer = null
 })
 
-//break button 
+//break button for showing the new break overlay
 breakBtn.addEventListener('click', function(){
     if (breakPopup.style.display === 'none') {
+        timer.innerText = '25:00'
+        stopTimer(second,minuteFocusTime)
         breakPopup.style.display = 'inline'
     } else { 
         breakPopup.style.display = 'none'
@@ -35,17 +45,32 @@ breakBtn.addEventListener('click', function(){
 }
 )
 
-function stopTimer() {
+//start break timer button
+startBreakTimerBtn.addEventListener('click', function(){
+    if (intervalTimer === null) {
+        updateTime(second,minuteBreak,timerBreak)
+        renderTime(second,minuteBreak,timerBreak)
+    }
+    }
+)
+
+stopBreakTimerBtn.addEventListener('click', function(){
+    if (intervalTimer) {
+        clearInterval(intervalTimer)
+        intervalTimer = null
+    }
+    }
+)
+
+function stopTimer(second,minute) {
     clearInterval(intervalTimer)
     intervalTimer = null
     second = 0
     minute = 25
-    startBtn.classList.remove('hidden')//show start button
-    //stopBtn.classList.add('hidden')//hide stop button
 }
 
 //updates the timer every second
-function updateTime() {
+function updateTime(second,minute,anyTimer) {
     intervalTimer = setInterval(function() {
         if(second > 0 && second < 60) {
             second -= 1
@@ -53,15 +78,15 @@ function updateTime() {
             minute = minute - 1
             second = 59
         } else {
-            stopTimer()
+            stopTimer(second,minute,anyTimer)
         }
-        renderTime()
+        renderTime(second,minute,anyTimer)
     }, 1000)
     
 }
 
 //renders the timer on the page
-function renderTime() {
+function renderTime(second,minute,anyTimer) {
     let minuteString = minute
     let secondString = second 
     if (secondString < 10) {
@@ -70,6 +95,6 @@ function renderTime() {
     if (minuteString < 10) {
         minuteString = `0${minuteString}`
     }
-    timer.innerText = `${minuteString}:${secondString}`
+    anyTimer.innerText = `${minuteString}:${secondString}`
 }
 
